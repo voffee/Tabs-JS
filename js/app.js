b74a122128bcefe96e244c4f09cd042538679419
Val <60014136+voffee@users.noreply.github.com>
2026-03-30 09:42:40 +0800

    JS completed.

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+const tabs = document.querySelector(`.tabs`);
+const tabButtons = document.querySelectorAll(`[role=tab]`);
+const tabPanels = document.querySelectorAll(`[role=tabpanel]`);
+
+function handleTabClick(e) {
+    // Hide all Tab Panels
+    tabPanels.forEach(panel =>  {
+        panel.hidden = true;
+    });
+    // Mark all tabs as unselected
+    tabButtons.forEach(tab => {
+        tab.ariaSelected = false;
+    });
+    // Mark the clicked tab as selected
+    e.currentTarget.ariaSelected = true;
+    // Find the associated tabpanel and show it
+    const { id } = e.currentTarget;
+    const showPanel = tabs.querySelector(`[aria-labelledby=${id}]`);
+    showPanel.hidden = false;
+
+}
+
+tabButtons.forEach(button => button.addEventListener(`click`, handleTabClick));
